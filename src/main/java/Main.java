8ebe53628340d3b0aeb67e4672b961878de1ee9e
CharlesMld <cmilliau@enssat.fr>
2023-09-19 18:16:06 +0200

    1er tp

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Main {
+    public static void main(String[] args){
+        Money instance = new Money(42, "EUR");
+        System.out.println(instance.toString());
+    }
+}
