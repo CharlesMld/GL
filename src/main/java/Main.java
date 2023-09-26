cf0a9896d11690801b8f059fbcb5403caaa2f752
Charles Milliaud <charles-edouard.milliaud@etudiant.univ-rennes1.fr>
2023-09-26 16:40:22 +0200

    Delete src/main/java directory

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-package demo;
-public class Main {
-    public static void main(String[] args){
-        Money instance = new Money(42, "EUR");
-        System.out.println(instance.toString());
-    }
-}
